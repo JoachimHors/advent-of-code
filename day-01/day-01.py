7ad446c229cf3e027a4652e802f7e65272dfc238
JoachimHors <jh.hors@gmail.com>
2023-12-03 23:21:24 +0100

    completed day-01 part 2

--- day-01/day-01.py
+++ day-01/day-01.py
@@ -5,7 +5,13 @@ with open("input.txt", "r") as file:
 
 sum = 0
 
+words_to_numbers = {"one": "1", "two": "2", "three": "3", "four": "4", "five": "5",
+                    "six": "6", "seven": "7", "eight": "8", "nine": "9"}
+
+
 for line in lines:
+    for word in words_to_numbers:
+        line = line.replace(word, word + words_to_numbers[word] + word)
     digits = re.findall(r"\d", line)
     sum += int(digits[0] + digits[-1])
 
