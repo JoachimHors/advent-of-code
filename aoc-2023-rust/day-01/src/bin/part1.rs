a7f00c65aea4e848eccc2cea7249778eed92c0ca
JoachimHors <jh.hors@gmail.com>
2023-12-08 16:51:20 +0100

    Completed day-01 part-1 in rust

---
+++ aoc-2023-rust/day-01/src/bin/part1.rs
@@ -0,0 +1,47 @@
+fn main() {
+    let input = include_str!("./input1.txt");
+    let output = part1(input);
+    dbg!(output);
+}
+
+fn part1(input: &str) -> u64 {
+    let _input: String = input.to_string();
+    let mut sum: u64 = 0;
+
+    for line in _input.lines() {
+        let mut first: char = 'a';
+        let mut last: char = 'a';
+        for character in line.chars() {
+            if character.is_digit(10) {
+                if first == 'a' {
+                    first = character;
+                }
+                last = character;
+            }
+        }
+        let concatenated = format!("{}{}", first, last);
+        if let Ok(value) = concatenated.parse::<u64>() {
+            sum += value;
+            println!("{}", line);
+            println!("{}", value);
+            println!("");
+        }
+    }
+
+    sum
+}
+
+// Tests
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_part_1() {
+        const TEST_INPUT: &str = "1abc2
+                                pqr3stu8vwx
+                                a1b2c3d4e5f
+                                treb7uchet";
+        let result = super::part1(TEST_INPUT);
+        assert_eq!(result, 142);
+    }
+}
